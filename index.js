ff45ab37a9921ed25de8e70bca35bb7c022aa28b
HaarikaR <hramadugu@gmail.com>
2021-09-20 19:01:38 -0700

    commit tree 065afdad82600678227dab2e54ed7ca740e461f3

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+
+
+let mod;
+let num3 = 24;
+let num4 = 5;
+mod = num3 % num4;
+
+let max;
+max = Math.max(20);
+
+let random ;
+random = Math.floor(Math.random() * 100000) + 1;
+
+
